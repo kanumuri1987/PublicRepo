d747a3bf8adabfc47bc8edfa705e5e4a7385edf7
Ubuntu <ubuntu@ip-172-31-45-101.us-east-2.compute.internal>
2020-07-07 21:22:16 +0000

    fist demo programs

---
+++ src/NewJavaProgram.java
@@ -0,0 +1,16 @@
+public class NewJavaProgram
+{
+
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(new App().getMessage());
+    }
+
+    private final String getMessage() {
+        return message;
+    }
+
+}
